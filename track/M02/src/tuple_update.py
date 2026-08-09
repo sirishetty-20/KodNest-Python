3ff816639425f68cef9d74090efbd90cb19dfa7d
Siri Shetty K <sirimagadh929@gmail.com>
2026-08-09 22:34:46 +0530

    update

---
+++ track/M02/src/tuple_update.py
@@ -0,0 +1,9 @@
+Course_name=input("Enter the course name:")
+Current_week=input("Enter the current week:")
+Course_status=input("Enter the course status:")
+#Creating a tuple
+Course_details=(Course_name,Current_week,Course_status)
+#updating the exixting tuple
+Updated_week=input("Enter the updated week:")
+Course_details=(Course_name,Updated_week,Course_status)
+print(Course_details)

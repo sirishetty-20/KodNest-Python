74cc50fde75ac42796184cbb6226767605e985f2
Siri Shetty K <sirimagadh929@gmail.com>
2026-08-10 17:27:47 +0530

    total

---
+++ track/M02/src/quantity.py
@@ -0,0 +1,6 @@
+def total_cost_invoice(price,quantity):
+    total=price*quantity
+    print("Total:",total)
+price=int(input("Enter the price:"))
+quantity=int(input("Enter the quantity:"))
+total_cost_invoice(price,quantity)

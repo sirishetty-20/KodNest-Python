720f1d80fdbc1e33e16584b6f7fb981a17d60e40
Siri Shetty K <sirimagadh929@gmail.com>
2026-08-18 15:20:58 +0530

    func

---
+++ track/M02/src/func2.py
@@ -0,0 +1,26 @@
+# def count_frequency():
+#     count=0
+#     for i in s:
+#         if i== 'a':
+#             count=count+1
+#     print(count)
+# s="salman"
+# count_frequency()
+
+
+# def count_frequency(name):
+#     count=0
+#     for i in name:
+#         if i=='a':
+#             count=count+1
+#     print(count)
+# count_frequency("banana")
+
+
+def count_frequency(str,target):
+    count=0
+    for i in str:
+        if i==target:
+            count=count+1
+    print(count)
+count_frequency("india","i")

046333fb6a03adb1a24beaa9259d14c346213e02
Siri Shetty K <sirimagadh929@gmail.com>
2026-08-19 15:48:47 +0530

    Class_object

--- track/M02/src/class_obj.py
+++ track/M02/src/class_obj.py
@@ -1,12 +1,21 @@
+# class Student:
+#     def __init__(self,roll_no,name,age,marks):
+#         self.roll_no=roll_no
+#         self.name=name
+#         self.age=age
+#         self.marks=marks
+#     def study(self):
+#         print(f"{self.name} is studying")
+# s1=Student(11,"Amith",22,85)
+# s1.study()
+# s2=Student(12,"Arun",23,90)
+# s2.study()
+
 class Student:
-    def __init__(self,roll_no,name,age,marks):
-        self.roll_no=roll_no
-        self.name=name
-        self.age=age
-        self.marks=marks
-    def study(self):
-        print(f"{self.name} is studying")
-s1=Student(11,"Amith",22,85)
-s1.study()
-s2=Student(12,"Arun",23,90)
-s2.study()
+    def study(self,roll_no,name,age,marks):
+         print(f"{name} is studying")
+s1=Student()
+s2=Student()
+s1.study(11,"Amith",22,85)
+s2.study(12,"Arun",23,90)
+   
